16cd47cd9d3a32c57f72913bf2d1311ca613a800
Chase Hawthorne <ch73168@gmail.com>
2016-04-25 20:47:40 -0600

    Added stock data and sample data

--- build/run-local.sh
+++ build/run-local.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+<<<<<<< Updated upstream
 /usr/local/spark/bin/pyspark --master local[8] Documents/kmeans.py /s/bach/n/under/owenrd/Documents/stock > logs.txts
+=======
+/usr/local/spark/bin/pyspark --master local[8] Documents/kmeans.py ~/stock-kmeans/stock > logs.txts
+>>>>>>> Stashed changes
